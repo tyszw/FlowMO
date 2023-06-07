9dfb2f88217ba765ec216c1fa4d8d00d18c0d5cf
tyszw <ttyyszw@gmail.com>
2023-06-07 16:19:47 +0900

    feat: add support for cross-validation

--- property_prediction/cv_with_tanimoto.py
+++ property_prediction/cv_with_tanimoto.py
@@ -4,7 +4,7 @@ Script for training a model to predict properties in the photoswitch dataset usi
 """
 
 import argparse
-
+import math
 import os
 import sys
 sys.path.append('..')  # to import from GP.kernels and property_predition.data_utils
@@ -14,28 +14,29 @@ from gpflow.mean_functions import Constant
 from gpflow.utilities import print_summary
 from matplotlib import pyplot as plt
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
 from GP.kernels import Tanimoto
 from data_utils import transform_data, TaskDataLoader, featurise_mols
 
 
-def main(path, task, representation, use_pca, n_trials, test_set_size, use_rmse_conf, precompute_repr):
+def main(path, task, representation, use_pca, n_splits, use_rmse_conf, precompute_repr):
     """
     :param path: str specifying path to dataset.
     :param task: str specifying the task. One of ['Photoswitch', 'ESOL', 'FreeSolv', 'Lipophilicity']
     :param representation: str specifying the molecular representation. One of ['SMILES, fingerprints, 'fragments', 'fragprints']
     :param use_pca: bool. If True apply PCA to perform Principal Components Regression.
-    :param n_trials: int specifying number of random train/test splits to use
-    :param test_set_size: float in range [0, 1] specifying fraction of dataset to use as test set
+    # :param n_splits: int specifying number of random train/test splits to use
     :param use_rmse_conf: bool specifying whether to compute the rmse confidence-error curves or the mae confidence-
     error curves. True is the option for rmse.
     :param precompute_repr: bool indicating whether to precompute representations or not.
     """
 
+    print('\nLoading data...')
     data_loader = TaskDataLoader(task, path)
     smiles_list, y = data_loader.load_property_data()
+    print('\nBeginning featurisation...')
     X = featurise_mols(smiles_list, representation)
 
     if precompute_repr:
@@ -60,46 +61,41 @@ def main(path, task, representation, use_pca, n_trials, test_set_size, use_rmse_
     def objective_closure():
         return -m.log_marginal_likelihood()
 
+    pred_list = np.empty(len(y))
+    var_list = np.empty(len(y))
     r2_list = []
     rmse_list = []
     mae_list = []
 
+    # define the spliting strategy for cross validation
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=0)
+    # get the number of test samples
+    n_test = math.ceil(len(y) / n_splits)
+
     # We pre-allocate arrays for plotting confidence-error curves
+    rmse_confidence_list = np.zeros((n_splits, n_test))
+    mae_confidence_list = np.zeros((n_splits, n_test))
 
-    _, _, _, y_test = train_test_split(X, y, test_size=test_set_size)  # To get test set size
-
-    # Photoswitch dataset requires 80/20 splitting. Other datasets are 80/10/10.
-
-    if task != 'Photoswitch':
-        split_in_two = int(len(y_test)/2)
-        n_test = split_in_two
-    else:
-        n_test = len(y_test)
-
-    rmse_confidence_list = np.zeros((n_trials, n_test))
-    mae_confidence_list = np.zeros((n_trials, n_test))
 
     print('\nBeginning training loop...')
 
-    for i in range(0, n_trials):
+    for i, (train_index, test_index) in enumerate(kf.split(X)):
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_set_size, random_state=i)
+        print(f'\nBeginning {i+1}th fold of {n_splits} fold cross validation...')
+
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
 
         if representation == 'SMILES':
 
-            np.savetxt(f'fixed_train_test_splits/{task}/X_train_split_{i}.txt', X_train, fmt="%s")
-            np.savetxt(f'fixed_train_test_splits/{task}/X_test_split_{i}.txt', X_test, fmt="%s")
-            np.savetxt(f'fixed_train_test_splits/{task}/y_train_split_{i}.txt', y_train)
-            np.savetxt(f'fixed_train_test_splits/{task}/y_test_split_{i}.txt', y_test)
+            np.savetxt(f'fixed_train_test_splits/{task}/{n_splits}cv_X_train_fold{i}.txt', X_train, fmt="%s")
+            np.savetxt(f'fixed_train_test_splits/{task}/{n_splits}cv_X_test_fold{i}.txt', X_test, fmt="%s")
+            np.savetxt(f'fixed_train_test_splits/{task}/{n_splits}cv_y_train_fold{i}.txt', y_train)
+            np.savetxt(f'fixed_train_test_splits/{task}/{n_splits}cv_y_test_fold{i}.txt', y_test)
 
         else:
-
-            if task != 'Photoswitch':
-
-                # Artificially create a 80/10/10 train/validation/test split discarding the validation set.
-                split_in_two = int(len(y_test)/2)
-                X_test = X_test[0:split_in_two]
-                y_test = y_test[0:split_in_two]
 
             y_train = y_train.reshape(-1, 1)
             y_test = y_test.reshape(-1, 1)
@@ -159,6 +155,8 @@ def main(path, task, representation, use_pca, n_trials, test_set_size, use_rmse_
             print("RMSE: {:.3f}".format(rmse))
             print("MAE: {:.3f}".format(mae))
 
+            pred_list[test_index] = y_pred.flatten()
+            var_list[test_index] = y_var.flatten()
             r2_list.append(score)
             rmse_list.append(rmse)
             mae_list.append(mae)
@@ -173,17 +171,26 @@ def main(path, task, representation, use_pca, n_trials, test_set_size, use_rmse_
         print("mean RMSE: {:.4f} +- {:.4f}".format(np.mean(rmse_list), np.std(rmse_list)/np.sqrt(len(rmse_list))))
         print("mean MAE: {:.4f} +- {:.4f}\n".format(np.mean(mae_list), np.std(mae_list)/np.sqrt(len(mae_list))))
 
+        with open(f'{task}/results/tanimoto_{n_splits}cv/result.txt', 'w') as f:
+            f.write(f'mean R^2: {np.mean(r2_list)} +- {np.std(r2_list)/np.sqrt(len(r2_list))}\n')
+            f.write(f'mean RMSE: {np.mean(rmse_list)} +- {np.std(rmse_list)/np.sqrt(len(rmse_list))}\n')
+            f.write(f'mean MAE: {np.mean(mae_list)} +- {np.std(mae_list)/np.sqrt(len(mae_list))}\n')
+        np.savetxt(f'{task}/results/tanimoto_{n_splits}cv/prediction.txt', pred_list)
+        np.savetxt(f'{task}/results/tanimoto_{n_splits}cv/variance.txt', var_list)
+
         # Plot confidence-error curves
 
-        confidence_percentiles = np.arange(1e-14, 100, 100/len(y_test))  # 1e-14 instead of 0 to stop weirdness with len(y_test) = 29
+        # confidence_percentiles = np.arange(1e-14, 100, 100/len(y_test))  # 1e-14 instead of 0 to stop weirdness with len(y_test) = 29
+        confidence_percentiles = np.arange(1e-14, 100+100/len(y_test), 100/len(y_test))
 
         # add by yszw
-        os.makedirs(f'{task}/results/tanimoto', exist_ok=True)
+        out_dir = f'{task}/results/tanimoto_{n_splits}cv'
+        os.makedirs(out_dir, exist_ok=True)
 
         if use_rmse_conf:
-
-            rmse_mean = np.mean(rmse_confidence_list, axis=0)
-            rmse_std = np.std(rmse_confidence_list, axis=0)
+            rmse_confidence_list[rmse_confidence_list == 0] = np.nan
+            rmse_mean = np.nanmean(rmse_confidence_list, axis=0)
+            rmse_std = np.nanstd(rmse_confidence_list, axis=0)
 
             # We flip because we want the most confident predictions on the right-hand side of the plot
 
@@ -202,15 +209,15 @@ def main(path, task, representation, use_pca, n_trials, test_set_size, use_rmse_
             plt.ylim([0, np.max(upper) + 1])
             plt.xlim([0, 100*((len(y_test) - 1) / len(y_test))])
             plt.yticks(np.arange(0, np.max(upper) + 1, 5.0))
-            plt.savefig(task + '/results/tanimoto/{}_confidence_curve_rmse.png'.format(representation))
+            plt.savefig(f'{out_dir}/{representation}_confidence_curve_rmse.png')
             plt.show()
 
         else:
 
             # We plot the Mean-absolute error confidence-error curves
-
-            mae_mean = np.mean(mae_confidence_list, axis=0)
-            mae_std = np.std(mae_confidence_list, axis=0)
+            mae_confidence_list[mae_confidence_list == 0] = np.nan
+            mae_mean = np.nanmean(mae_confidence_list, axis=0)
+            mae_std = np.nanstd(mae_confidence_list, axis=0)
 
             mae_mean = np.flip(mae_mean)
             mae_std = np.flip(mae_std)
@@ -225,7 +232,7 @@ def main(path, task, representation, use_pca, n_trials, test_set_size, use_rmse_
             plt.ylim([0, np.max(upper) + 1])
             plt.xlim([0, 100 * ((len(y_test) - 1) / len(y_test))])
             plt.yticks(np.arange(0, np.max(upper) + 1, 5.0))
-            plt.savefig(task + '/results/tanimoto/{}_confidence_curve_mae.png'.format(representation))
+            plt.savefig(f'{out_dir}/{representation}_confidence_curve_rmse.png')
             plt.show()
 
 
@@ -242,10 +249,8 @@ if __name__ == '__main__':
                              'One of [SMILES, fingerprints, fragments, fragprints].')
     parser.add_argument('-pca', '--use_pca', type=bool, default=False,
                         help='If True apply PCA to perform Principal Components Regression.')
-    parser.add_argument('-n', '--n_trials', type=int, default=1,
+    parser.add_argument('-n', '--n_splits', type=int, default=1,
                         help='int specifying number of random train/test splits to use')
-    parser.add_argument('-ts', '--test_set_size', type=float, default=0.2,
-                        help='float in range [0, 1] specifying fraction of dataset to use as test set')
     parser.add_argument('-rms', '--use_rmse_conf', type=bool, default=True,
                         help='bool specifying whether to compute the rmse confidence-error curves or the mae '
                              'confidence-error curves. True is the option for rmse.')
@@ -254,5 +259,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    main(args.path, args.task, args.representation, args.use_pca, args.n_trials, args.test_set_size, args.use_rmse_conf,
+    main(args.path, args.task, args.representation, args.use_pca, args.n_splits, args.use_rmse_conf,
          args.precompute_repr)
